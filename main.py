668c792fc222bf4164a4224ba1611ff621db537e
snova301 <snova301.ysja@gmail.com>
2021-05-06 06:24:54 +0000

    2nd

--- main.py
+++ main.py
@@ -1,12 +1,71 @@
 import torch
-from torchtext.datasets import AG_NEWS
-
+# from torchtext.datasets import AG_NEWS
+from transformers import BertJapaneseTokenizer, BertForMaskedLM
 
 def main_sample():
-    train_iter = AG_NEWS(split='train')
-    print(train_iter)
-    print(type[train_iter])
-    print(next(train_iter))
+    # train_iter = AG_NEWS(split='train')
+    # print(train_iter)
+    # print(type[train_iter])
+    # print(next(train_iter))
+    tokenizer = BertJapaneseTokenizer.from_pretrained('cl-tohoku/bert-base-japanese-whole-word-masking')
+
+    # Tokenize input
+    text = 'テレビでサッカーの試合を見る。'
+    tokenized_text = tokenizer.tokenize(text)
+    print(tokenized_text)
+
+    # Mask a token that we will try to predict back with `BertForMaskedLM`
+    masked_index = 2
+    tokenized_text[masked_index] = '[MASK]'
+    # ['テレビ', 'で', '[MASK]', 'の', '試合', 'を', '見る', '。']
+    print(tokenized_text)
+
+    # Convert token to vocabulary indices
+    indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
+    # [571, 12, 4, 5, 608, 11, 2867, 8]
+    print(indexed_tokens)
+
+    # Convert inputs to PyTorch tensors
+    tokens_tensor = torch.tensor([indexed_tokens])
+    # tensor([[ 571,   12,    4,    5,  608,   11, 2867,    8]])
+    print(tokens_tensor)
+
+    # # Load pre-trained model
+    # model = BertForMaskedLM.from_pretrained('cl-tohoku/bert-base-japanese-whole-word-masking')
+    # model.eval()
+
+    # # Predict
+    # with torch.no_grad():
+    #     outputs = model(tokens_tensor)
+    #     predictions = outputs[0][0, masked_index].topk(5) # 予測結果の上位5件を抽出
+
+    # # Show results
+    # for i, index_t in enumerate(predictions.indices):
+    #     index = index_t.item()
+    #     token = tokenizer.convert_ids_to_tokens([index])[0]
+    #     print(i, token)
+
+    from transformers import BertForSequenceClassification, Trainer, TrainingArguments
+
+    model = BertForSequenceClassification.from_pretrained("bert-large-uncased")
+
+    training_args = TrainingArguments(
+        output_dir='./results',          # output directory
+        num_train_epochs=3,              # total # of training epochs
+        per_device_train_batch_size=16,  # batch size per device during training
+        per_device_eval_batch_size=64,   # batch size for evaluation
+        warmup_steps=500,                # number of warmup steps for learning rate scheduler
+        weight_decay=0.01,               # strength of weight decay
+        logging_dir='./logs',            # directory for storing logs
+    )
+
+    trainer = Trainer(
+        model=model,                         # the instantiated 🤗 Transformers model to be trained
+        args=training_args,                  # training arguments, defined above
+        train_dataset=train_dataset,         # training dataset
+        eval_dataset=test_dataset            # evaluation dataset
+    )
+
 
 
 if __name__ == '__main__':
--- readme.md
+++ readme.md
@@ -5,8 +5,19 @@
 
 
 
+
+# 使用したもの
+bertはpytorchモデル
+
+
+
+
 # 参考文献
 - [RNN（LSTM）でのネットワーク構成・データセット作成・学習方法　まとめ - Qiita](https://qiita.com/MENDY/items/99da56f61f9af51dda15)
 - [【入門】自然言語処理でできることをいくつかPythonで実装してみる！ - スタビジ](https://toukei-lab.com/natural-language-python)
 - [RNNとLSTM（Long Short Term Memory）の違いと特徴 - AUC技術ブログ](https://www.acceluniverse.com/blog/developers/2019/07/lstm.html)
+- [BERTとは｜Googleが誇る自然言語処理モデルの仕組み、特徴を解説 - Ledge](https://ledge.ai/bert/)
+- [【PyTorch】BERTの使い方 - 日本語pre-trained modelsをfine tuningして分類問題を解く- Qiita](https://qiita.com/kenta1984/items/7f3a5d859a15b20657f3)
+- [PyTorchで日本語BERTによる文章分類＆Attentionの可視化を実装してみた - Qiita](https://qiita.com/m__k/items/e312ddcf9a3d0ea64d72)
+- [bert](https://huggingface.co/transformers/index.html)
 - [ちびむすドリル小学生](https://happylilac.net/mu1605121851.html)
